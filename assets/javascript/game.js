7fd49acebf96d5a2a14cb34762baabb803647f0a
yoli <yramirezdesigns@gmail.com>
2019-02-10 22:30:32 -0500

    tried to fix but still not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 function crystalGame (){
-    var gemRandom = ["1", "2", "3", "4",]; //"5", "6", "7", "8", "9", "10", "11", "12"];
-    var randomNum = [19-20];//120];
-    var gemTotalAmount = 0;
+    var gemRandom = ["1", "2", "3", "4","5", "6", "7", "8", "9", "10", "11", "12"];
+    var randomNum = [19-120];
+    var gemTotalAmount = "";
     var wins = 0;
     var losses = 0;
     var winsElements = document.getElementById("winsNum");
@@ -13,28 +13,31 @@ function crystalGame (){
     var rubyElements = document.getElementById("rubyGem");
     var sappireElements = document.getElementById("sapphireGem");
     
-    const WriteGemTotal = {
+    const writeGemTotal = {
         lines: function() {return diamondGem += emeraldGem += rubyGem += sapphireGem}   
             gemsElements.addEventListener("click", function(e) {
                 gemsElements.innerHTML = WriteGemTotal.lines();
                 dimondElement[Math.floor(Math.random() * dimondElement.ength)];
                 emeraldElements[Math.floor(Math.random() * emeraldElement.ength)];
                 rubyElements[Math.floor(Math.random() * rubyElements.ength)];
-                sappireElements[Math.floor(Math.random() * sappireElements.ength)];
-            }
+                sappireElements[Math.floor(Math.random() * sappireElements.ength)];            }
         //randomize number for gems.
-        var imageButton = document.onclick= function(event){
-            gemRandom[Math.floor(Math.random() * gemRandom.length)];
-            for (var i = 0; i < gemRandom.length; i++);
-            gemTotalAmount (dimondElement += emeraldElements += rubyElements += sappireElements);
-            gemsElements.innerText=gemTotalScore;
-            }
-    }
-    //to randomize randomNum (what the computer selects)
-    Function randomComputerNum(){
-        Var randomizeNum =document.getElementById(guessElements).innerHTML = Math.floor(Math.random() * 2) + 1;
-        
-    }
+            function imageButton(){
+                var imageBut = document.onclick= function(event){
+                    gemRandom[Math.floor(Math.random() * gemRandom.length)];
+                    for (var i = 0; i < gemRandom.length; i++);
+                    gemTotalAmount (dimondElement += emeraldElements += rubyElements += sappireElements);
+                    gemsElements.innerText=gemTotalScore;
+                    }
+                }
+            } 
+        // imageButton();
+         //to randomize randomNum (what the computer selects)
+        function ComputerNum () {
+            Var randomNum =document.getElementById(guessElements).innerHTML = Math.floor(Math.random() * 2) + 1;    
+        }
+    // writeGemTotal();
+
             if (gemRandom === randomNum){
                 wins++;
                 winsElements.innerText=wins;
@@ -49,7 +52,9 @@ function crystalGame (){
             
             else {
                 function reset (){
-            
+                gemTotalAmount = 0;
+                randomComputerNum();
+
                 }
                    
             }
@@ -66,6 +71,7 @@ function crystalGame (){
             }
 
 
-    
+    }
 
 }
+crystalGame ();
